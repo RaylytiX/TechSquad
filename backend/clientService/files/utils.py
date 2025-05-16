13341668105cdf09292de7561e36e33c5b207023
Nikita Yuriev <nikita.yuriev2004@gmail.com>
2025-05-16 20:46:36 +0300

    fix save file

--- backend/clientService/files/utils.py
+++ backend/clientService/files/utils.py
@@ -1,16 +1,18 @@
 import uuid
 import aiofiles
 from fastapi import UploadFile
+from backend.configs.config import settings
 
 async def save_file(file: UploadFile):
     fileEXT = file.filename.split('.')[-1].lower()
     fileID = uuid.uuid4().hex
-    path_to_image = f"backend/media/{fileID + '.' + fileEXT}"
+    path_to_image = f"{settings.FILE_SAVE_FOLDER}/{fileID + '.' + fileEXT}"
     try:
         async with aiofiles.open(path_to_image, 'wb') as f:
             while contents := await file.read(1024 * 1024):
                 await f.write(contents)
     except Exception as e:
+        print(e)
         return None
     finally:
         await file.close()
